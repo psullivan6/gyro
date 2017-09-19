d8992c9533ffac4f61f98d9bbb23d5150b59ab55
Patrick Sullivan <patrick.sullivan@union.co>
2017-09-19 18:51:24 -0400

    WIP - basic canvas masked alpha rotation (manual triggering)

---
+++ public/js/Math.js
@@ -0,0 +1,3 @@
+export const getRadians = (degrees) => {
+  return degrees * (Math.PI / 180);
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 import Rotator from './Rotator.js';
+import { getRadians } from './Math.js';
 
 const rotator = new Rotator();
 
@@ -11,12 +12,75 @@ const alphaDOM = document.querySelector('[data-rotate-alpha]');
 const betaDOM = document.querySelector('[data-rotate-beta]');
 const gammaDOM = document.querySelector('[data-rotate-gamma]');
 
-rotator.on('rotate', function(values) {
+
+
+
+// =============================================================================
+// Canvas Manipulation
+// =============================================================================
+const canvas = document.getElementById('Water');
+
+// Get canvas sizing from the css values
+var cWidth = parseInt(window.getComputedStyle(canvas).width, 10);
+var cHeight = parseInt(window.getComputedStyle(canvas).height, 10);
+
+// Double the canvas size for retina
+canvas.width = cWidth * 2;
+canvas.height = cHeight * 2;
+
+const context = canvas.getContext("2d");
+
+
+const image = document.createElement('img');
+image.src = '/images/water.jpg';
+image.width = '100%';
+
+const getYOffset = (radians, adjacent) => {
+  return Math.tan(radians) * adjacent;
+}
+
+const drawMask = (rotation) => {
+  const yValue = canvas.height / 2;
+  const yOffset = getYOffset(rotation, (canvas.height / 2));
+
+  // Clear and Save
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.save(); // [NOTE] this is crucial
+
+  // Clipping Mask
+  context.beginPath();
+  context.moveTo(0, canvas.height);                // bottom left
+  context.lineTo(0, yValue + yOffset);             // top left
+  context.lineTo(canvas.width, yValue - yOffset);  // top right
+  context.lineTo(canvas.width, canvas.height);     // bottom right
+  context.closePath();
+  context.clip();
+
+  // Image Background
+  context.drawImage(image, 0, 0, canvas.width, canvas.height);
+  context.restore(); // [NOTE] this is crucial
+}
+
+
+
+
+// =============================================================================
+// Rotator logic
+// =============================================================================
+const rotate = (values) => {
+  const alpha = Math.floor(values.alpha);
+  const beta = Math.floor(values.beta);
+  const gamma = Math.floor(values.gamma);
+
   // console.log('ROTATE', rotator.initialValues);
-  alphaDOM.innerHTML = Math.floor(values.alpha);
-  betaDOM.innerHTML = Math.floor(values.beta);
-  gammaDOM.innerHTML = Math.floor(values.gamma);
-});
+  alphaDOM.innerHTML = alpha;
+  betaDOM.innerHTML = beta;
+  gammaDOM.innerHTML = gamma;
+
+  drawMask(getRadians(alpha));
+};
+
+rotator.on('rotate', rotate);
 
 rotator.on('init', function(values) {
   console.log('ROTATE - INIT', values);
@@ -24,3 +88,35 @@ rotator.on('init', function(values) {
   betaDOMInitial.innerHTML = Math.floor(values.beta);
   gammaDOMInitial.innerHTML = Math.floor(values.gamma);
 });
+
+
+
+
+
+
+
+
+
+
+// =============================================================================
+// Manual Adjustment (Testing)
+// =============================================================================
+// context.clearRect(0, 0, canvas.width, canvas.height);
+
+const adjustment = document.getElementById('adjust');
+
+let adjustmentValues = {
+  alpha : 0,
+  beta  : 0,
+  gamma : 0
+};
+
+const handleAdjustment = () => {
+  console.log('HANDLE ADJUSTMENT');
+
+  adjustmentValues.alpha += 1;
+
+  rotate(adjustmentValues);
+}
+
+adjustment.addEventListener('click', handleAdjustment);
